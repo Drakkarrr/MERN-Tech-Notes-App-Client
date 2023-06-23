4a1a0eefe80de77a7efb6e41206750d4ba326051
Drakkarrr <junrey404@gmail.com>
2023-06-23 10:27:45 +0800

    added routes and connect to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,61 @@
-import { Routes, Route } from "react-router-dom"
-import Layouts from "./components/Layouts"
-import Public from "./components/Public"
-import Login from "./features/auth/Login"
-import DashLayout from "./components/DashLayout"
-import Welcome from "./features/auth/Welcome"
-import NotesList from "./features/notes/NotesList"
-import UsersList from "./features/users/UsersList"
+import { Routes, Route } from 'react-router-dom'
+import Layout from './components/Layouts'
+import Public from './components/Public'
+import Login from './features/auth/Login';
+import DashLayout from './components/DashLayout'
+import Welcome from './features/auth/Welcome'
+import NotesList from './features/notes/NotesList'
+import UsersList from './features/users/UsersList'
+import EditUser from './features/users/EditUser'
+import NewUserForm from './features/users/NewUserForm'
+import EditNote from './features/notes/EditNote'
+import NewNote from './features/notes/NewNote'
+import Prefetch from './features/auth/Prefetch'
+import PersistLogin from './features/auth/PersistLogin'
+import RequireAuth from './features/auth/RequireAuth'
+import { ROLES } from './config/roles'
+import useTitle from './hooks/useTitle';
 
 function App() {
+  useTitle('Dan D. Repairs')
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Layouts />}>
-          <Route index element={<Public />} />
-          <Route path="login" element={<Login />} />
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* public routes */}
+        <Route index element={<Public />} />
+        <Route path="login" element={<Login />} />
 
-          <Route path="dash" element={<DashLayout />}>
-            <Route index element={<Welcome />} />
-            <Route path="notes" element={<NotesList />} />
-            <Route path="users" element={<UsersList />} />
+        {/* Protected Routes */}
+        <Route element={<PersistLogin />}>
+          <Route element={<RequireAuth allowedRoles={[...Object.values(ROLES)]} />}>
+            <Route element={<Prefetch />}>
+              <Route path="dash" element={<DashLayout />}>
+
+                <Route index element={<Welcome />} />
+
+                <Route element={<RequireAuth allowedRoles={[ROLES.Manager, ROLES.Admin]} />}>
+                  <Route path="users">
+                    <Route index element={<UsersList />} />
+                    <Route path=":id" element={<EditUser />} />
+                    <Route path="new" element={<NewUserForm />} />
+                  </Route>
+                </Route>
+
+                <Route path="notes">
+                  <Route index element={<NotesList />} />
+                  <Route path=":id" element={<EditNote />} />
+                  <Route path="new" element={<NewNote />} />
+                </Route>
+
+              </Route>{/* End Dash */}
+            </Route>
           </Route>
+        </Route>{/* End Protected Routes */}
 
-        </Route>
-      </Routes>
-    </>
-  )
+      </Route>
+    </Routes >
+  );
 }
 
-export default App
+export default App;
